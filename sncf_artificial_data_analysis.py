82d9d30e5629ff6e897fd816df0b374e480e8ef6
PiteDur <58419072+PiteDur@users.noreply.github.com>
2026-04-15 09:44:28 +0200

    sncf data analysis - init v0

---
+++ sncf_artificial_data_analysis.py
@@ -0,0 +1,36 @@
+"""
+SNCF ARTIFICIAL DATA ANALYSIS
+
+We propose here an analysis of the artificial data created with copilot 
+(see data/synthetic/train_delays_synthetic_2025_2026.csv)
+
+OBJECTIVE:
+* analyse the propagation of delays over the train network
+* identify the main factors influencing delays' occurence
+* predict the delay of a train given its characteristics and the current state of the network 
+"""
+
+
+"""
+PACKAGES
+"""
+
+
+
+
+
+
+
+
+
+"""
+DATA AND PATHS
+"""
+
+
+
+
+
+"""
+DESCRITPIVE STATISTICS
+"""
